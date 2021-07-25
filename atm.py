395212d28da5fdd3594316302be4bf13554769c2
Manasa1010 <64597788+Manasa1010@users.noreply.github.com>
2021-07-25 16:51:41 +0530

    Add files via upload
    atm

---
+++ atm.py
@@ -0,0 +1,18 @@
+class atm(object):
+    def __init__(self,userAtmCardNo,pinNumber):
+        self.userAtmCardNo=userAtmCardNo
+        self.pinNumber=pinNumber
+
+    def tellUserAtmCardNo(self):
+        print("User's atm card number is"+str(self.userAtmCardNo))
+
+    def tellPinNumber(self):
+        print("Pin Munber of user is"+str(self.pinNumber))
+    def cashWithDrawl():
+        print()
+
+    def balanceEnquiry():
+        print()
+def main():
+    detail1=atm()
+
