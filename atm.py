f5ff68fb38ed0786c02bf1c54beb0dc3b48346c9
Manasa1010 <64597788+Manasa1010@users.noreply.github.com>
2021-07-25 21:05:24 +0530

    Add files via upload
    atm

--- atm.py
+++ atm.py
@@ -1,4 +1,4 @@
-class atm(object):
+class Atm(object):
     def __init__(self,userAtmCardNo,pinNumber):
         self.userAtmCardNo=userAtmCardNo
         self.pinNumber=pinNumber
@@ -8,11 +8,25 @@ class atm(object):
 
     def tellPinNumber(self):
         print("Pin Munber of user is"+str(self.pinNumber))
-    def cashWithDrawl():
-        print()
+    def cashWithDrawl(self):
+        print("cash you with drawl is 10000")
 
-    def balanceEnquiry():
-        print()
+    def balanceEnquiry(self):
+        print("you have 50000 in your account")
+
 def main():
-    detail1=atm()
+    cardNum=int(input("Enter your card number: "))
+    pinNum=int(input("Enter your pin number: "))
+    num=int(input("Enter your choice(1 for cash withdrawl 2 for balance enquiry): "))
+    account=Atm(cardNum,pinNum)
+    if(num==1):
+        account.cashWithDrawl()
+    elif(num==2):
+        account.balanceEnquiry()
+    else:
+        print("Wrong input")
+
+main()
+
+    
 
